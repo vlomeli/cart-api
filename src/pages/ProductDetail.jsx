cb299207ef74c5637c0f2afabbb81b261bd95a92
Victor Lomeli <victorlomeli2023@gmail.com>
2025-05-18 14:09:19 -0700

    added back & cart  buttons / altered css

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,10 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { fetchProductById } from "../utils/api";
-import '../styles/ProductDetail.css';
+import { useNavigate } from "react-router-dom";
+
+import "../styles/ProductDetail.css";
 
 function ProductDetail() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [product, setProduct] = useState(null);
   const [loading, setLoading] = useState(null);
   const [error, setError] = useState(null);
@@ -26,13 +29,19 @@ function ProductDetail() {
   if (loading) return <div>Loading product...</div>;
   if (error) return <div>Error: {error}</div>;
   if (!product) return <p>Product not found.</p>;
-  
+
   return (
-    <div className="product-detail">
-      <h2>{product.name}</h2>
-      <img src={product.image} alt={product.name} />
-      <p>{product.description}</p>
-      <p>Price: ${product.price}</p>
+    <div className="product-detail-container">
+      <button onClick={() => navigate(-1)} className="back-button">
+        ← Back
+      </button>
+      <div className="product-detail">
+        <h2>{product.name}</h2>
+        <img src={product.image} alt={product.name} />
+        <p>{product.description}</p>
+        <p>Price: ${product.price}</p>
+        <button className="add-to-cart-button">Add to Cart 🛒</button>
+      </div>
     </div>
   );
 }
